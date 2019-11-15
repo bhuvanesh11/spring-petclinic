4218711da3b6cb46b29f05ba1938a9e619d330aa
bhuvanesh11 <bhuvanesh0311@gmail.com>
2019-11-15 06:41:32 +0100

    Skypefiles

---
+++ Skypemvn.py
@@ -0,0 +1,6 @@
+from skpy import *
+
+#contacts = []
+sk = Skype('bhuvanesh0311@gmail.com', 'Londonparis@1')
+ch = sk.chats["19:de0f09a0026240e69df3a4d6a9f18d96@thread.skype"]
+ch.sendMsg('Maven Build Completed successfully')
